4684e67439dcd083bce182dd5529897eb33ff71d
Meghana17-10 <meghanajala07@gmail.com>
2026-05-03 21:04:39 +0530

    remove number to get minimum number

---
+++ minNum.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class minNum{
+ public static void main(String args[]){
+  Scanner sc = new Scanner(System.in);
+  int n = sc.nextInt();
+  String s = String.valueOf(n);
+  int min = Integer.MAX_VALUE;
+  for(int i=0;i<s.length();i++){
+   String str = s.substring(0,i)+s.substring(i+1);
+   int m = Integer.parseInt(str);
+   min = Math.min(min,m);
+  }
+  System.out.println(min);
+ }
+}
