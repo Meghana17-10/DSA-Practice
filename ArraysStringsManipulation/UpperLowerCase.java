198628c2f39aaadb294abe101708ba866fbcb25e
Meghana17-10 <meghanajala07@gmail.com>
2026-06-29 21:16:35 +0530

    changed folder name

---
+++ ArraysStringsManipulation/UpperLowerCase.java
@@ -0,0 +1,21 @@
+/*Given a Sentence  , for each word If the first letter in a
+string is capital letter then change the full string to capital letters,
+else change the full string to small letters.
+
+
+sample test case:
+input = Welcome tO Opentext
+output= WELECOME to OPENTEXT
+*/
+import java.util.*;
+class UpperLowerCase{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        char[] arr = str.toCharArray();
+        if(Character.isLowerCase(arr[0])) str = str.toLowerCase();
+        if(Character.isUpperCase(arr[0])) str = str.toUpperCase();
+        
+        System.out.print(str);
+    }
+}
